82d5c5f024ea3ba1b25edfc1cb10d7bccbc58f69
j9650 <hzj5142@aci-lgn-006.acib.production.int.aci.ics.psu.edu>
2020-05-10 18:02:21 -0400

    change one line

--- cal_rmsd.py
+++ cal_rmsd.py
@@ -10,6 +10,7 @@ for line in f:
     exe_time.append(float(line.strip().split()[-1]))
 
 assert len(exe_time) == 4
+assert len(exe_time) == 4
 
 print(str(exe_time[0])+' '+str(exe_time[1])+' '+str(exe_time[2])+' '+str(exe_time[3]))
 
